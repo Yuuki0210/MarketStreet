86a89bd5587c347627246026fb7b3e68d94652d1
Yuuki0210 <kni.hose@gmail.com>
2021-10-13 23:36:56 +0000

    editommit

--- db/migrate/20211010171402_create_items.rb
+++ db/migrate/20211010171402_create_items.rb
@@ -2,10 +2,10 @@ class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
       t.string :name ,null: false
-      t.string :image_id ,null: false
       t.text :explanation ,null: false
       t.integer :price ,null: false
       t.boolean :is_active ,null: false, default: false
+      t.integer :genre_id, null: false
 
       t.timestamps
     end
