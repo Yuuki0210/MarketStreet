77b4519b2bed4b5441c5922260e00a26590e3794
Yuuki0210 <kni.hose@gmail.com>
2021-10-14 03:43:18 +0000

    drop_admin.migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,35 +1,36 @@
 Rails.application.routes.draw do
 
-  # namespace :admin do
-  #   devise_for :admins, controllers: {
-  #     sessions: 'admin/admins/sessions',
-  #     registrations: 'admin/admins/registrations',
-  #     passwords: 'admin/admins/passwords'
-  #   }
-  # resources :items, except: [:destroy]
-  # resources :genres, only: [:index, :create, :edit, :update, :destroy]
-  # resources :customers, except: [:create, :destroy, :new]
-  # resources :orders, only: [:index, :show, :update]
-  # end
+  namespace :admin do
+    devise_for :admins, controllers: {
+      sessions: 'admin/admins/sessions',
+      registrations: 'admin/admins/registrations',
+      passwords: 'admin/admins/passwords'
+    }
+  resources :items, except: [:destroy]
+  resources :genres, only: [:index, :create, :edit, :update, :destroy]
+  resources :customers, except: [:create, :destroy, :new]
+  resources :orders, only: [:index, :show, :update]
+  end
 
-  # scope module: 'customer' do
-  # devise_for :customers, controllers: {
-  #   sessions: 'customer/customers/sessions',
-  #   registrations: 'customer/customers/registrations',
-  #   passwords: 'customer/customers/passwords'
-  # }
-  # resources :items, only: [:index, :show]
-  # resources :cart_items, only: [:create, :index, :update, :destroy]
-  # delete '/cart_items' => 'cart_items#all_destroy', as: 'all_destroy'
-  # post 'orders/check' => 'orders#check', as: 'check'
-  # get 'orders/thanks' => 'orders#thanks', as: 'thanks'
-  # resources :orders, only: [:new, :create, :index, :show]
-  # resources :customers, only: [:edit, :update]
-  # get 'customers/mypage' => 'customers#show', as: 'mypage'
-  # get 'customers/delete' => 'customers#delete', as: 'delete'
-  # patch 'customers/deleted' => 'customers#deleted', as: 'deleteed'
-  # root to: 'homes#top'
-  # get 'about' => 'homes#about'
-  # end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope module: 'customer' do
+  devise_for :customers, controllers: {
+    sessions: 'customer/customers/sessions',
+    registrations: 'customer/customers/registrations',
+    passwords: 'customer/customers/passwords'
+  }
+  resources :items, only: [:index, :show]
+  resources :cart_items, only: [:create, :index, :update, :destroy]
+  delete '/cart_items' => 'cart_items#all_destroy', as: 'all_destroy'
+  post 'orders/check' => 'orders#check', as: 'check'
+  get 'orders/thanks' => 'orders#thanks', as: 'thanks'
+  resources :orders, only: [:new, :create, :index, :show]
+  resources :customers, only: [:edit, :update]
+  get 'customers/mypage' => 'customers#show', as: 'mypage'
+  get 'customers/delete' => 'customers#delete', as: 'delete'
+  patch 'customers/deleted' => 'customers#deleted', as: 'deleteed'
+  root to: 'homes#top'
+  get 'about' => 'homes#about'
+  end
+
 end
+
