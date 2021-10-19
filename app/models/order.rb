3babd9c65158e47cbd2b07173815112ab4c13910
Yuuki0210 <kni.hose@gmail.com>
2021-10-19 07:41:28 +0000

    order_index

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,4 +4,5 @@ class Order < ApplicationRecord
   has_many :items, through: :order_items
   has_many :order_items
   enum payment_method: { "クレジットカード":0, "銀行振込":1 }
+  enum status: { "入金待ち":0, "入金確認":1, "発送準備中":2, "発送済み":3 }
 end
