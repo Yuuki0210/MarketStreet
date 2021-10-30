865ddc2bdefe6205e6591706406e07fa39865e20
Yuukio210 <kni.hose@gmail.com>
2021-10-30 02:20:46 +0000

    last

--- app/controllers/customer/homes_controller.rb
+++ app/controllers/customer/homes_controller.rb
@@ -2,7 +2,6 @@ class Customer::HomesController < ApplicationController
 
   def top
     @items = Item.where(is_active: 1)
-    @random = Item.where(is_active: 1).order("RANDOM()").limit(5)
   end
 
   def about
