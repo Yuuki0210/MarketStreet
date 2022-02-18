71b68a7f2af8f6994e3821356a02c70f9ab32b80
Yuukio210 <kni.hose@gmail.com>
2022-02-18 12:26:02 +0000

    admin_items

--- app/controllers/customer/homes_controller.rb
+++ app/controllers/customer/homes_controller.rb
@@ -1,8 +1,8 @@
 class Customer::HomesController < ApplicationController
 
   def top
-    @items = Item.where(is_active: 1)
-    
+     @items = Item.where(is_active: 1)
+     @random = Item.where(is_active: 1).order("RANDOM()").limit(5)
   end
 
   def about
